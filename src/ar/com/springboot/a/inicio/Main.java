9057262e1cde86388a019767b7067721db894770
Juan Lima <juan_jpl@hotmail.com>
2025-09-05 19:50:51 -0300

    start project

---
+++ src/ar/com/springboot/a/inicio/Main.java
@@ -0,0 +1,12 @@
+package ar.com.springboot.a.inicio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Comenzamos con java!");
+		
+	}
+
+}
