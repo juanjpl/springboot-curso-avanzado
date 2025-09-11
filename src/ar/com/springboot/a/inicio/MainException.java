1e2610878ee5fd4db74a930b54b8f5439cd80a96
juanjpl <juan_jpl@hotmail.com>
2025-09-11 10:31:20 -0300

    upload file

---
+++ src/ar/com/springboot/a/inicio/MainException.java
@@ -0,0 +1,17 @@
+package ar.com.springboot.a.inicio;
+
+import java.util.ArrayList;
+
+public class MainException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<String> ar = new ArrayList<>();
+		
+		ar.add("Hola");
+		
+		System.out.println(ar.get(10));
+	}
+
+}
