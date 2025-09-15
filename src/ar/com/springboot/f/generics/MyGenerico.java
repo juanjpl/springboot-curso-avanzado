7f69fb9b01b5b45c8843cd6d28e3bccea8d3d934
juanjpl <juan_jpl@hotmail.com>
2025-09-15 11:27:49 -0300

    upload files

--- src/ar/com/springboot/f/generics/MyGenerico.java
+++ src/ar/com/springboot/f/generics/MyGenerico.java
@@ -1,5 +1,21 @@
 package ar.com.springboot.f.generics;
 
-public class MyGenerico {
+public class MyGenerico<T> {
 
+	private T campoGenerico;
+	private Integer campoEntero;
+	
+	
+	public MyGenerico(T campoGenerico, Integer campoEnterio) {
+		this.campoGenerico = campoGenerico;
+		this.campoEntero = campoEntero;
+	}
+	
+	public T getCampoGenerico() {
+		return  campoGenerico;
+	}
+	
+	public void setCampoGenerico(T campoGenerico) {
+		this.campoGenerico = campoGenerico;
+	}
 }
